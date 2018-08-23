a5ce4492589dfd4e6f96e4adf38520f662961e75
Mauricio Caroca <maucaroca@gmail.com>
2018-08-22 21:28:57 -0300

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "students#index"
   resources :students
+  get "pages/landing"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
